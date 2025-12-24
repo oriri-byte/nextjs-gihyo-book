1f7c688b30daa6bd7e9a0f89f71a991f0ddf795e
oriri-byte <179485773+oriri-byte@users.noreply.github.com>
2025-12-24 19:17:58 +0900

    fix(registry): libディレクトリの階層の変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import StyledComponentsRegistry from './lib/registry';
+import StyledComponentsRegistry from '../lib/registry';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
