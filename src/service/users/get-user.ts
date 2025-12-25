802961d679eed129b948c665e3f6327490302b4e
oriri-byte <179485773+oriri-byte@users.noreply.github.com>
2025-12-26 04:01:41 +0900

    feat(get-user): ユーザを取得するAPIクライアントを実装
    - ユーザidを用いてサーバからユーザ情報を取得するAPIクライアントを実装

---
+++ src/service/users/get-user.ts
@@ -0,0 +1,35 @@
+import type { ApiContext, User } from 'types';
+import { fetcher } from '../../utils/fetcher';
+
+export type GetUserParams = {
+  id: number;
+};
+
+/**
+ * ユーザAPI
+ * @param context APIコンテキスト
+ * @param params パラメータ
+ * @returns ユーザ
+ */
+const getUser = async (context: ApiContext, { id }: GetUserParams): Promise<User> => {
+  /**
+   * ユーザAPI
+   * サンプルレスポンス
+   * {
+   *  "id": 1,
+   *  "username": "sampleuser",
+   *  "displayName": "Sample User",
+   *  "email": "sampleuser@example.com",
+   *  "profileImageUrl": "/users/1.png",
+   *  "description": "LoremIpsum is simply dummy text of printing and typesetting industry"
+   * }
+   */
+  return await fetcher(`${context.apiRootUrl.replace(/\/$/g, '')}/users/${id}`, {
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
+};
+
+export default getUser;
