3432c638f79ebe3a4d3e3605aea7ddcb8817afd9
oriri-byte <179485773+oriri-byte@users.noreply.github.com>
2025-12-26 04:46:21 +0900

    feat : rewritesを導入
    リクエストされたurlを別の宛先パスにマッピングする

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,16 @@ const nextConfig: NextConfig = {
   compiler: {
     styledComponents: true,
   },
+  async rewrites() {
+    return [
+      {
+        // ex. /api/proxy
+        source: `${process.env.NEXT_PUBLIC_API_BASE_PATH}/:match*`,
+        // ex. http://localhost:8000
+        destination: `${process.env.API_BASE_URL}/:match*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
